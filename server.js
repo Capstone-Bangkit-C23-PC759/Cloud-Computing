18746707281cd48d55ab581f58d0351ed4f50c4a
adio ihsan <adioihsan@gmail.com>
2023-05-30 13:52:44 +0700

    setup mysql connection

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 'use strict'
-
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require("./routes")
-
+global.sqlPool = require("./app/connections/database.connection")
 const init = async () => {
-
     const server = Hapi.server({
         port: 3000,
         host: 'localhost'
     });
     server.route(routes)
-
     await server.start();
-    console.log('Server running on %s', server.info.uri);
 };
-
 process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
